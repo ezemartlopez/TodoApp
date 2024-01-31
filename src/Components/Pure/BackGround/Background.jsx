e7bdc18df1d54be40030eda69e176aa1d37ee9cc
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-01-31 16:39:39 -0300

    modificado ahora usa variables globales de estilo.

--- src/Components/Pure/BackGround/Background.jsx
+++ src/Components/Pure/BackGround/Background.jsx
@@ -1,25 +1,10 @@
-import { connectThemeDevice } from '../../../StoreRedux/conectors/conectFunction';
 import './background.style.css';
 
-const imageBackground = {
-  light:{
-    mobile: './src/assets/Background/ModeLigthMobile.png',
-    desktop: './src/assets/Background/ModeLightDesktop.png'
-  },
-  night:{
-    mobile: './src/assets/Background/ModeDarkMobile.png',
-    desktop: './src/assets/Background/ModeDarkDesktop.png'
-  }
-}
-
-
-function Background({mode, device}) {
-  const image = imageBackground[mode][device];
+function Background(){
   return (
-    <div>
-        <img src={image} alt="background_image" className='image-background'/>  
+    <div className='background-container-image'>
     </div>
   )
 }
 
-export default connectThemeDevice(Background);
+export default Background;
