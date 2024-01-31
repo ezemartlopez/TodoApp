6b1c21c5a96b79791ee78a4b58873af59d5fdc45
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-01-31 16:42:28 -0300

    pensando si elimino este componente.

--- src/Components/Pure/IconMode/IconMode.jsx
+++ src/Components/Pure/IconMode/IconMode.jsx
@@ -1,15 +1,27 @@
 import { connectTheme } from "../../../StoreRedux/conectors/conectFunction";
 const LIGHT = 'light';
 
+function toggleThemeFunction() {
+  const htmlElement = document.documentElement;
+  const currentTheme = htmlElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+
+  htmlElement.setAttribute("data-theme", newTheme);
+}
+
 function IconMode({mode, toggleTheme}) {
   const styles = {
     fontSize: '35px',
     cursor: 'pointer'
   }
+  const ButtontoggleTheme = () =>{
+    toggleTheme();
+    toggleThemeFunction();
+  }
  return (
   <i className="material-icons" 
     style={styles} 
-    onClick={toggleTheme}>
+    onClick={ButtontoggleTheme}>
     {mode===LIGHT? 'mode_night':'light_mode' }
   </i>
   )
