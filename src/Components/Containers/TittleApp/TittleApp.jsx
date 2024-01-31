3efde1707517066a77ab2b8c0b235ba5f110980b
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-01-31 16:38:14 -0300

    se modifico ya que no requiere mas del estado.

--- src/Components/Containers/TittleApp/TittleApp.jsx
+++ src/Components/Containers/TittleApp/TittleApp.jsx
@@ -1,13 +1,18 @@
 import './tittleapp.style.css';
-import IconMode from '../../Pure/IconMode/IconMode';
+import { toggleTheme } from './functions';
 
 function TittleApp() {
+  const iconName = () =>{
+    const htmlElement = document.documentElement;
+    const currentTheme = htmlElement.getAttribute("data-theme");
+    return currentTheme==='light'? 'light_mode': 'mode_night';
+  }
   return (
       <div className='tittle-app-container'>
         <span className='tittle-app'>TODO</span>
-        <IconMode></IconMode>
+        <i className="material-icons icon-mode-change" onClick={toggleTheme}> {iconName()}</i>
       </div>
   )
 }
 
-export default TittleApp
+export default TittleApp;
